ec5a007d8577b8e90b07c04ad7c8cbcff64aa59c
Martina Novoselac <novoselac.martina@gmail.com>
2023-05-26 00:45:27 +0200

    dodavavanje registracije /prijave

--- singup.php
+++ singup.php
@@ -1,21 +1,61 @@
 <?php
 
-include 'dbh.php';
-
-$Ime = $_POST['Ime'];
-$Prezime = $_POST['Prezime'];
-$Adresa = $_POST['Adresa'];
-$Grad = $_POST['Grad'];
-$Email = $_POST['Email'];
-$Lozinka = $_POST['Lozinka'];
-
-
-
-$sql = "insert into korisnici (Ime, Prezime, Adresa, Grad, Email, Lozinka)
- values ('$Ime','$Prezime', '$Adresa', '$Grad', '$Email', '$Lozinka')";
-$result = $conn->query($sql);
-
-header('Location: naslovnica.html');
-
+include 'header.php';
 
 ?>
+
+
+<?php 
+ if (isset($_SESSION['id'])) {
+	 echo $_SESSION['id'];
+ } else {
+	 echo "Prijava je uspjela!";
+ }
+?>
+	
+	<br><br><br>
+	<?php
+	
+	$url= "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+	if (strpos($url,'error=empty') !==false){
+		echo "Postoji prazno obvezno  polje!";
+	}
+	elseif (strpos($url,'error=email') !==false){
+		echo "Email već postoji u bazi nije ga moguće ponovno koristiti prilikom registracije!";
+	}
+	
+	if (isset($_SESSION['id'])) {
+	 echo "Već si registriran!";
+	} else {
+	 echo "<form action='includes/singup.inc.php' method='POST'>
+		<input type='text' name='Ime' placeholder='Ime'>
+		</br></br>
+		<input type='text' name='Prezime' placeholder='Prezime'>
+		</br></br>
+		<input type='text' name='Adresa' placeholder='Adresa'>
+		</br></br>
+		<input type='text' name='Grad' placeholder='Grad'>
+		</br></br>
+		<input type='text' name='Email' placeholder='Email'>
+		</br></br>
+		<input type='password' name='Lozinka' placeholder='Lozinka'>
+		</br></br>
+
+			<button type='submit'> Registriraj se</button>
+		</form>";
+	}
+	?>
+	
+
+
+
+</br></br>
+
+<form action="includes/logout.inc.php">
+	<button>ODJAVA</button>
+</form>
+
+
+
+</body>
+</html>
